aec306452150327a8957902c3761d45a02e14b6e
Moises Sacal <moisbo@gmail.com>
2020-12-16 11:18:08 +1100

    added try catch for awaits

--- index.js
+++ index.js
@@ -1,22 +1,27 @@
 const DescriboFrameController = require('./api/controllers/DescriboFrameController');
 const DescriboFrameService = require('./api/services/DescriboFrameService');
 const auth = require('./auth');
-const { HookConfig } = require('sails-hook-redbox-hook-config-utils');
+const {HookConfig} = require('sails-hook-redbox-hook-config-utils');
 const config = require('./hook.json');
 
 module.exports = function (sails) {
   return {
     initialize: async function (cb) {
-      const name = require('./package').name;
-      const hook = HookConfig.base(name, config);
-      await HookConfig.angular({
-        hookName: hook.name,
-        angularDest: hook.angularDest,
-        angularOrigin: hook.angularDest,
-        angularTmpDest: hook.angularTmpDest
-      });
-      HookConfig.views(sails.config.appPath, hook.name);
-      cb();
+      try {
+        const name = require('./package').name;
+        const hook = HookConfig.base(name, config);
+        await HookConfig.angular({
+          hookName: hook.name,
+          angularDest: hook.angularDest,
+          angularOrigin: hook.angularDest,
+          angularTmpDest: hook.angularTmpDest
+        });
+        HookConfig.views(sails.config.appPath, hook.name);
+        cb();
+      } catch (e) {
+        sails.log.error(e);
+        cb();
+      }
     },
     //If each route middleware do not exist sails.lift will fail during hook.load()
     routes: {
